531f33a5c2ed67a64c3f136b0e09d5c1c89c3870
victorcobo2 <victorcobo22@gmail.com>
2022-03-16 14:54:47 -0500

    Mas busquedas por filtro

---
+++ src/models/CON110H.js
@@ -0,0 +1,58 @@
+const { type } = require('express/lib/response');
+const mongoose = require('mongoose');
+console.clear()
+mongoose.pluralize(null) //Con este evitamos que mongo ponga una S al final
+const ciudadSchema = mongoose.Schema({
+    ubicacion: {
+        type: String,
+        //required: true,
+        default: ""
+      },
+    direct: {
+        type: String,
+        required: true
+    },
+    subdirect:{
+        type:String,
+        required: true
+    },
+    codCiu:{
+        dptCiu:{
+            type: String,
+            unique: true
+        },
+        ciuCiu:{
+            type: String,
+            //default:"xd"
+            unique: true
+        },
+        type: Object,
+        unique: true
+    },
+    nombre:{
+
+        "nombre1":{
+            type: String,
+            default:""
+        },
+        "nombre2":{
+            type: String,
+            default:""
+        }
+    },
+    pais:{
+        type: String,
+        required: true
+    },
+    actbarrios:{
+        type: String,
+        //required: true
+        default: "" //El valor por defecto sera este, si no se envia nada.
+    },
+    increm:{
+        type: Number,
+        required: true
+    }
+  },{versionKey: false}); //Con este evitamos que mongo agrege una variable llamada __V
+
+ module.exports = mongoose.model('ciudad', ciudadSchema);
---
+++ src/validators/CON110H.js
@@ -0,0 +1,68 @@
+const { check } = require('express-validator');
+const { validateResult } = require('../helpers/validateHelper')
+const { validarExistencia } = require('./global')
+
+let ubicacion ,direct, subdirect, paisCiu, actbarrios;
+
+const validateCreate = [
+
+    check('ubicacion')
+    .isLength({max:20})
+    
+
+
+    ,check('direct','subdirect')
+    .exists()
+    .isLength({max:10})
+     
+    ,check('pais')
+    .exists()
+    .not()
+    .isEmpty()
+    .isLength({max:3})
+
+    ,check('codCiu')
+    .exists()
+    .isLength({min: 2})
+    .isObject()
+
+    ,check('increm')
+    .exists()
+    .not()
+    .isEmpty()
+    .isNumeric()
+    .isLength({max:1}),
+
+    (req, res, next) =>{
+        req.body.actbarrios = actbarrios
+        req.body.ubicacion = ubicacion
+        validateResult(req, res, next)
+    }
+]
+
+const ValidateEdit = [
+     check('ubicacion')
+     .isLength({max:20}),
+
+
+    check('direct','subdirect')
+    .isLength({max:10})
+     
+    ,check('pais')
+    .isLength({max:3})
+
+    
+
+    ,check('increm')
+    //.isNumeric()
+    .isLength({max:1}),
+
+    (req, res, next) =>{
+        req.body.actbarrios = actbarrios
+        req.body.ubicacion = ubicacion
+        validateResult(req, res, next)
+    }
+]
+
+
+module.exports = {validateCreate, ValidateEdit}
